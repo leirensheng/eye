14abbb787ca39687994b4d1a599f826d7b8c9328
leirensheng <leirensheng@163.com>
2021-09-24 16:37:09 +0800

    feat: 样式修改

--- components/history-item/history-item.vue
+++ components/history-item/history-item.vue
@@ -5,7 +5,7 @@
       <div class="right">
         <div class="name">{{ item.productName }}</div>
         <div class="bottom">
-          <div class="date">{{ $formatTime(item.createDate) }}</div>
+          <div class="date">{{ $formatTime(item.createDate, true) }}</div>
           <div class="status" :class="item.genStatus === 0 && 'light'">
             {{ getStatus(item.genStatus) }}
           </div>
@@ -16,8 +16,8 @@
     <div class="wrap frame frame-light" :class="!isShowFrame && 'hidden'">
       <div class="left"></div>
       <div class="right">
-        <div class="name">{{fackName}}</div>
-        <div class="name">{{fackName}}</div>
+        <div class="name">{{ fackName }}</div>
+        <div class="name">{{ fackName }}</div>
         <div class="bottom">
           <div class="date">{{ fackDate }}</div>
           <div class="status">
@@ -33,8 +33,7 @@
 export default {
   data() {
     return {
-      fackName:
-        "占位",
+      fackName: "占位",
       fackStatus: "需要花费1分钟生成",
       fackDate: "2021-01-01 19:50:50",
     };
@@ -78,6 +77,7 @@ export default {
 .history-item {
   box-shadow: 0px 0px 12rpx 0px rgba(0, 0, 0, 0.15);
   padding: 32rpx 24rpx 32rpx 16rpx;
+  border-radius: 10rpx;
   .wrap {
     display: flex;
   }
@@ -99,16 +99,17 @@ export default {
         margin: 12rpx 0;
         color: $grey;
         background: $grey;
-        &:first-child{
+        &:first-child {
           margin-top: 6rpx;
         }
-        &:nth-child(2){
+        &:nth-child(2) {
           margin-bottom: 22rpx;
         }
       }
 
       .bottom {
-        .date,.status  {
+        .date,
+        .status {
           line-height: 1;
           height: 28rpx;
           margin: 6rpx 0;
